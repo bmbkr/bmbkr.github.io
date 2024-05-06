8fd5057971543d37b23668fff8899f2a0be31e60
Brandon Baker <brandon@niea.me>
2024-05-06 14:17:52 -0400

    furguson :)

---
+++ sketch.js
@@ -0,0 +1,60 @@
+let catImage;
+
+function preload() {
+  catImage = loadImage('furguson.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+class Face {
+  constructor() {
+    this.size = 50 + Math.random() * 150
+    this.x = Math.random() * width;
+    this.y = -this.size;
+    this.speed = Math.random() * 3 + 1;
+    this.direction = Math.random() - 0.5;
+    this.rotationalSpeed = (Math.random() - 0.5) * 0.05;
+    this.rotation = 0;
+  } 
+
+  display() {
+    this.y += this.speed;
+    this.x += this.direction * this.speed;
+
+    imageMode(CENTER);
+    push();
+
+    // tint(255, 255, 255, 250);
+
+    translate(this.x, this.y);
+    rotate(this.rotation);
+    image(catImage, 0, 0, this.size, this.size);
+    pop();
+
+    this.rotation += this.rotationalSpeed;
+  }
+
+  isOffScreen() {
+    return this.y > height + this.size;
+  }
+}
+
+const faces = [];
+
+function draw() {
+  background(0);
+
+  if (frameCount % 10 === 0) {
+    faces.push(new Face());
+  }
+
+  faces.forEach(face => face.display());
+
+  faces.forEach(face => {
+    if (face.isOffScreen()) {
+      faces.splice(faces.indexOf(face), 1);
+    }
+  });
+}
