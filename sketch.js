eae6d836d8bf5649b591de34da553d3f33d82714
Brandon Baker <brandon@niea.me>
2024-05-06 14:20:32 -0400

    fullscreen

--- sketch.js
+++ sketch.js
@@ -58,3 +58,18 @@ function draw() {
     }
   });
 }
+
+
+// Wait 10 seconds then go fullscreen
+setTimeout(() => {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+});
